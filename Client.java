9eab149ec8d2b560509bc5dbe6f8db5e09612fa7
byan1197 <Bondyan97@gmail.com>
2017-03-22 23:55:33 -0400

    updated client

--- Client.java
+++ Client.java
@@ -2,29 +2,31 @@ import java.io.*;
 import java.net.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing;
+import javax.swing.*;
 
 public class Client extends JFrame{
     private JTextField userText;
     private JTextArea chatWindow;
     private ObjectOutputStream output;
     private ObjectInputStream input;
-    private String message = "":
+    private String message = "";
     private String serverIP;
     private Socket connection;
-
     public Client (String host){
         super("Client");
         userText = new JTextField();
         userText.setEditable(false);
-        uesrText.addActionListener(
+
+
+        userText.addActionListener(
             new ActionListener(){
-                public void actionPerformed(ActionEvent event{
-                    sendData(event.getActionCommand());
+                public void actionPerformed(ActionEvent event){
+                    sendMessage(event.getActionCommand());
                     userText.setText("");
-                })
+                }
             }
         );
+
         add(userText, BorderLayout.NORTH);
         chatWindow = new JTextArea();
         add(new JScrollPane(chatWindow), BorderLayout.CENTER);
@@ -32,13 +34,87 @@ public class Client extends JFrame{
         setVisible(true);
     }
 
-    //connect to server`
-    private void startRunning(){
+    public void startRunning(){
         try{
-
+            connectToServer();
+            setupStreams();
+            whileChatting();
         }catch(EOFException eofException){
-            showMessage("\n Client terminated connection")
+            showMessage("\n Client terminated connection");
+        }catch(IOException ioException){
+            ioException.printStackTrace();
+        }finally{
+            closeCrap();
         }
     }
 
+    private void connectToServer() throws IOException{
+        showMessage("Attempting connection...\n");
+        connection = new Socket(InetAddress.getByName(serverIP), 6789);
+        showMessage("Connected to:" + connection.getInetAddress().getHostName());
+
+    }
+
+    private void setupStreams() throws IOException {
+        output = new ObjectOutputStream(connection.getOutputStream());
+        output.flush();
+        input = new ObjectInputStream(connection.getInputStream());
+        showMessage("\n Streams are good to go");
+    }
+
+    private void whileChatting() throws IOException{
+        ableToType(true);
+        do{
+            try{
+                message = (String) input.readObject();
+                showMessage("\n" +message);
+            }catch(ClassNotFoundException cnfException){
+                showMessage("\n Wrong object type");
+            }
+        }while(!message.equals("SERVER - END"));
+    }
+
+    private void closeCrap(){
+        showMessage("Closing all the streams n stuff");
+        ableToType(false);
+        try{
+            output.close();
+            input.close();
+            connection.close();
+        }catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+
+    private void sendMessage(String message){
+        try{
+            output.writeObject("CLIENT - "+message);
+            output.flush();
+            showMessage("\nCLIENT -" +message);
+        }catch(IOException ioException){
+            chatWindow.append("\n something s wrong!");
+        }
+    }
+
+    //change/update chatWindow
+    private void showMessage(final String m){
+        SwingUtilities.invokeLater(
+            new Runnable(){
+                public void run(){
+                    chatWindow.append(m);
+                }
+            }
+        );
+    }
+
+    //typing into the textbox
+    private void ableToType(final boolean tof){
+        SwingUtilities.invokeLater(
+            new Runnable(){
+                public void run(){
+                    userText.setEditable(tof);
+                }
+            }
+        );
+    }
 }
